243da320769d624acbb0db4e9848b357a6402651
Nkepers <nick.kepers.coder@gmail.com>
2022-01-03 23:10:04 -0700

    restructured files with new directories to make code cleaner and work more functionally.

---
+++ routes/apiRoute.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const { createNewNote } = require("../lib/notes");
+const fs = require("fs");
+
+router.get("/notes", (req, res) => {
+    fs.readFile("./db/db.json", (error, notes) => {
+        if (error) {
+            return res.json(error)
+        }
+        notes = JSON.parse(notes)
+        res.json(notes)
+    });
+});
+
+router.post("/notes", (req, res) => {
+    fs.readFile("./db/db.json", (error, notes) => {
+        if (error) {
+            return res.json(error)
+        }
+        notes = JSON.parse(notes)
+       
+
+        req.body.id = notes.length.toString();
+    
+        const newNotes = createNewNote(req.body, notes)
+        res.json(newNotes)
+    });
+});
+
+router.delete("/notes/:id", (req, res) => {
+    const noteId = req.params.id
+    console.log(noteId)
+
+    fs.readFile("./db/db.json", (error, notes) => {
+        if (error) {
+            return console.log(error)
+        }
+        notes = JSON.parse(notes)
+
+        notes = notes.filter(val => val.id !== noteId)
+
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (error, data) => {
+            if (error) {
+                return error
+            }
+            res.json(notes)
+        })
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 //json dependencies
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-
-
-// Asynchronous Processes
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+const PORT = process.env.PORT || 3001;
+const htmlRoute = require('./routes/htmlRoute.js');
+const apiRoute = require('./routes/apiRoute')
 
 
 // Deploy server
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -21,64 +17,69 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-// "GET" API Route
-app.get('/api/notes', function(req, res) {
-    fs.readFile('./db/db.json').then(function(data) {
-        notes = [].concat.apply(JSON.parse(data))
-        res.json(notes);
-    })
-});
-
-
-//  "POST" API Route
-app.post('/api/notes', function(req, res) {
-    const note = req.body;
-    fs.readFile('./db/db.json').then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        note.id = notes.length + 1
-        notes.push(note);
-        return notes
-    }).then(function(notes) {
-        writeFileAsync('./db/db.json', JSON.stringify(notes))
-        res.json(note);
-    })
-});
-
-
-// "DELETE" API Route
-app.delete('/api/notes/:id', function(req, res) {
-    const noteDelete = parseInt(req.params.id);
-    readFileAsync('./db/db.json').then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        const newNote = []
-        for (let i = 0; i < notes.length; i++) {
-            if(noteDelete !== notes[i].id) {
-                newNote.push(notes[i])
-            }
-        }
-        return newNote
-    }).then(function(notes) {
-        writeFileAsync('./db/db.json', JSON.stringify(notes))
-        res.send('Note saved successfully!');
-    })
-})
-
-
-// Routes for HTML
-app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// Routing
+app.use('/api', apiRoute);
+app.use('/', htmlRoute)
 
 
 // Server is listening
 app.listen(PORT, function() {
     console.log('App is listening on PORT:' + PORT);
 });
+
+
+// // "GET" API Route
+// app.get('/api/notes', function(req, res) {
+//     fs.readFile('./db/db.json').then(function(data) {
+//         notes = [].concat.apply(JSON.parse(data))
+//         res.json(notes);
+//     })
+// });
+
+
+// //  "POST" API Route
+// app.post('/api/notes', function(req, res) {
+//     const note = req.body;
+//     fs.readFile('./db/db.json').then(function(data) {
+//         const notes = [].concat(JSON.parse(data));
+//         note.id = notes.length + 1
+//         notes.push(note);
+//         return notes
+//     }).then(function(notes) {
+//         writeFileAsync('./db/db.json', JSON.stringify(notes))
+//         res.json(note);
+//     })
+// });
+
+
+// // "DELETE" API Route
+// app.delete('/api/notes/:id', function(req, res) {
+//     const noteDelete = parseInt(req.params.id);
+//     readFileAsync('./db/db.json').then(function(data) {
+//         const notes = [].concat(JSON.parse(data));
+//         const newNote = []
+//         for (let i = 0; i < notes.length; i++) {
+//             if(noteDelete !== notes[i].id) {
+//                 newNote.push(notes[i])
+//             }
+//         }
+//         return newNote
+//     }).then(function(notes) {
+//         writeFileAsync('./db/db.json', JSON.stringify(notes))
+//         res.send('Note saved successfully!');
+//     })
+// })
+
+
+// // Routes for HTML
+// app.get('/notes', function(req, res) {
+//     res.sendFile(path.join(__dirname, './public/notes.html'));
+// });
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
